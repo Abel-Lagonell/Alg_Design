ac70298b85d3099ff1b51fe1f18bd85356aa4e99
Abel-Lagonell <alagonell1730@floridapoly.edu>
2023-02-21 22:44:59 -0500

    v 0.2

--- MiniProject 1/Node.py
+++ MiniProject 1/Node.py
@@ -1,33 +1,62 @@
 import random
+import math
 
 # Initialize the Nodes
 class Node:
     #Constructor
     def __init__(self):
-        self.x = random.randint(0, 1000)
-        self.y = random.randint(0, 1000)
-        setCost()
-        self.visited = False
+        self.__x = random.randint(0, 1000)
+        self.__y = random.randint(0, 1000)
+        self.setCost()
+        self.__visited = False
     
     #String representation of the Node
     def __str__(self):
-        return "City: (" + str(self.x) + ", " + str(self.y) + ") Cost: " + str(self.cost)
+        return "City: (" + str(self.__x) + ", " + str(self.__y) + ") Cost: " + str(self.__cost)
     
     #Setting the cost of the node based on the location
     def setCost(self):
-        # calculate cost based on location of city using switch statement
+        if (self.inRadius() == 0):
+            self.__cost = random.uniform(0.1, 0.5)
+        elif (self.inRadius() == 1):
+            self.__cost = random.uniform(2, 5)
+            self.__cost = random.uniform(5, 7)
+        else:
+            self.__cost = random.uniform(1, 2)
 
     #Setting the node as visited
     def setVisited(self):
-        self.visited = True
+        self.__visited = True
 
     #GET FUNCTIONS
 
     def getCost(self):
-        return self.cost
+        return self.__cost
 
     def getX(self):
-        return self.x
+        return self.__x
     
     def getY(self):
-        return self.y
+        return self.__y
+
+    def getVisited(self):
+        return self.__visited
+
+    #Distance between two nodes
+    def getDistance(self, node):
+        return math.sqrt((self.__x - node.getX())**2 + (self.__y - node.getY())**2)
+    
+    #Checks if node is in the radius of the price points and returns the price tier
+    def inRadius(self):
+        xLeft = (self.__x - 200)**2
+        xQuad1 = (self.__x - 800)**2
+        xQuad4 = (self.__x - 650)**2
+        yTop = (self.__y - 700)**2
+        yBottom = (self.__y - 200)**2
+        yLeft = (self.__y - 500)**2
+        if (xLeft + yLeft <= 50**2):
+            return 0
+        elif (xQuad1 + yTop <= 50**2):
+            return 1
+        elif (xQuad4 + yBottom <= 50**2):
+            return 2
