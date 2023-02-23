e23ccda7259b54393bf312e06313c139c6a5504a
CaptainBlast096 <jaleelr96@gmail.com>
2023-02-22 21:30:19 -0500

    Update Graph.py
    Added comments on creating graphs.  Jaleel will relocate to Algorithm of Set Cover

--- MiniProject 1/Graph.py
+++ MiniProject 1/Graph.py
@@ -1,3 +1,21 @@
-import plotly.express as px
-fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
-fig.show()
+import plotly.express as px #For the visual graph
+import pandas as pd #panda library is responsible for the DataFrame
+
+"""
+Template for Scatter Plot
+    df = px.data.iris() # iris is a pandas DataFrame
+    fig = px.scatter(df, x="sepal_width", y="sepal_length")
+    fig.show()
+
+data_frame is a 2D data structure like rows and columns of a table that is mutable
+    Synatx is : pandas.DataFrame(data, index, columns)
+
+Useful Functions:
+    COLOR_DISCRETE_SEQUENCE - modifys colors
+    COLOR enables modification of the points
+    OPACITY allows you to change transparency of point
+
+Objective:
+    Make two graphs, one with an X-axis named Budget from $0-$50 and dynamic Y-axis  named Coverage
+    Second graph contains the same X-axis but constant Y-axis will be named Radius from 0-5
+"""
