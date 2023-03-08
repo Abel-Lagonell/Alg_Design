b38e4f362277fd117f07a42a2cac68b324e8b31f
CaptainBlast096 <jaleelr96@gmail.com>
2023-03-08 18:53:09 -0500

    My shitty code
    In the Grid class I added a return to the set cover algorithm. In Graph class I color coded the algorithms and tried making a legends.

--- MiniProject 1/Graph.py
+++ MiniProject 1/Graph.py
@@ -3,7 +3,7 @@ import Node
 from Grid import Grid
 #Shout out to my boy Abel couldn't have done this Graph without these two classes excited to have done some work that helped -
 class Graph:
-    def __init__(self, budget=50, radius=5, type=1):
+    def __init__(self, budget=50, radius=5, type=0):
         self.grid = Grid(budget=budget, radius=radius, type=type)
         self.circle_x = 10  # x-coordinate of the center of the circle
         self.circle_y = 10  # y-coordinate of the center of the circle
@@ -11,36 +11,48 @@ class Graph:
         self.budget = budget
         self.enclosed_cost = 0 
     
-    #def find_nodes_randomly(self):
-    #    self.grid.randomAlgorithm(self)
-    
     def plot(self):
         # Generate a scatter plot of all nodes in the grid
         x = [node.getX() for node in self.grid.getNodes()]
         y = [node.getY() for node in self.grid.getNodes()]
         cost = [node.getCost() for node in self.grid.getNodes()]
-        fig = go.Figure(data=go.Scatter(x=x, y=y, mode='markers', text=cost, hovertemplate='(%{x}, %{y})<br>Cost: %{text:.2f}<extra></extra>')) 
+        
+        # Create a color map to color code each algorithm
+        colors = {'Random Algorithm': 'blue', 'Pure Greedy Algorithm': 'green', 'Greedy Set Cover Algorithm': 'red'}
+        
+        # Initialize the figure
+        fig = go.Figure()
+        
+        # Add a scatter trace for all nodes in the grid
+        fig.add_trace(go.Scatter(x=x, y=y, mode='markers', marker=dict(color='gray'), text=cost, 
+                                hovertemplate='(%{x}, %{y})<br>Cost: %{text:.2f}<extra></extra>', name='All Nodes'))
+        
+        # Add a trace for each algorithm
+        for i, (name, nodes) in enumerate([('Random Algorithm', self.grid.randomAlgorithm()), 
+                            ('Pure Greedy Algorithm', self.grid.pureGreedyAlgorithm()), 
+                            ('Greedy Set Cover Algorithm', self.grid.greedySetCoverAlgorithm())]):
+            
+            # Draw circles for the nodes selected by the algorithm
+            self.draw_circle(fig, nodes, self.budget, color=colors[name])
+
+            
+            # Add a legend for the algorithm
+            fig.add_trace(go.Scatter(x=[], y=[], mode='markers', marker=dict(color=colors[name]), 
+                              name=name, showlegend=True))
         
         # Customize the layout of the scatter plot
-        fig.update_layout(title="Node Scatter Plot",xaxis_title="Coverage",yaxis_title="Radius", height=600, width=800, margin=dict(l=50, r=50, b=50, t=50, pad=4),
-        #xaxis=dict(range=[0,50]), # Controls the range of the x-axis to an extent 
-        #yaxis=dict(range=[0,5]),  # Controls the range of the y-axis to an extent
-        )
+        fig.update_layout(title="Node Scatter Plot", xaxis_title="Coverage", yaxis_title="Radius", height=600, width=800, margin=dict(l=50, r=50, b=50, t=50, pad=4),legend=dict(title='Algorithms', yanchor='top', y=1.0, xanchor='left', x=0.1))
+                        #xaxis=dict(range=[0,50]), # Controls the range of the x-axis to an extent 
+                        #yaxis=dict(range=[0,5]),  # Controls the range of the y-axis to an extent
         
-        #nodes_to_enclose = self.grid.getNodesToEnlose(self.budget)
-        #if nodes_to_enclose:
-        #    self.draw_circle(fig, nodes_to_enclose, self.budget)
-#
-        #    # Show the plot
-        #    fig.show()
-
-        self.draw_circle(fig, self.grid.randomAlgorithm(), self.budget)
-        self.draw_circle(fig, self.grid.pureGreedyAlgorithm(), self.budget)
-        self.draw_circle(fig, self.grid.greedySetCoverAlgorithm(), self.budget)
         fig.show()
 
-    def draw_circle(self, fig, nodes:list[Node.Node], budget:int): #Create separate functions for each algorithm and then call them from this function
+
+    def draw_circle(self, fig, nodes: list[Node.Node], budget: int, color: str):
         # calculate the center of the circle
+        if nodes is None:
+            print("Error: nodes parameter is None")
+            return
         center_x = sum(node.getX() for node in nodes) / len(nodes)
         center_y = sum(node.getY() for node in nodes) / len(nodes)
 
@@ -51,10 +63,9 @@ class Graph:
         self.circle_y = center_y
 
         for node in nodes:
-            fig.add_shape(type="circle", xref="x", yref="y", x0=node.getX()-self.circle_radius, y0=node.getY()-self.circle_radius, x1=node.getX()+self.circle_radius, y1=node.getY()+self.circle_radius, line=dict(color="red", width=2, dash='dash')) #Visually encircles nodes
+            fig.add_shape(type="circle", xref="x", yref="y", x0=node.getX()-self.circle_radius, y0=node.getY()-self.circle_radius, x1=node.getX()+self.circle_radius, y1=node.getY()+self.circle_radius, line=dict(color=color, width=2, dash='dash')) #Visually encircles nodes
             fig.add_annotation(x=self.circle_x, y=self.circle_y,text=f'Budget: {budget:.2f}<br>Enclosed cost: {self.enclosed_cost:.2f}',font=dict(size=16), showarrow=False, bgcolor = 'white', bordercolor = 'black', borderwidth = 2, borderpad = 4) #Adds the textbox to the graph
-            # Draw a circle around the highlighted nodes
-           # if (node.getX() - self.circle_x) ** 2 + (node.getY() - self.circle_y) ** 2 <= self.circle_radius ** 2:
+
                 
               
 
