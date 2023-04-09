208f9d2bcf48c3faab22db3340e59c856b77f93a
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-04-09 16:47:10 -0400

    Update TimeComplexity.py
    Added one new function
    ~getAverageTimeDynamicProgrammingAlgorithm
    Purpose: Get the average of Dynamic function in Grid c;ass

--- MiniProject_1/TimeComplexity.py
+++ MiniProject_1/TimeComplexity.py
@@ -10,7 +10,7 @@ class TimeComplexity:
         for i in range(10):
             start_time = time.time()
             grid = Grid(10, 50)
-            nodes = grid.randomAlgorithm()
+            nodes = grid.Random()
             end_time = time.time()
             total_time += end_time - start_time
         return total_time/10
@@ -20,7 +20,7 @@ class TimeComplexity:
         for i in range(10):
             start_time = time.time()
             grid = Grid(10, 50)
-            nodes = grid.pureGreedyAlgorithm()
+            nodes = grid.Greedy()
             end_time = time.time()
             total_time += end_time - start_time
         return total_time/10
@@ -30,7 +30,17 @@ class TimeComplexity:
         for i in range(10):
             start_time = time.time()
             grid = Grid(10, 50)
-            nodes = grid.greedySetCoverAlgorithm()
+            nodes = grid.SetCover()
+            end_time = time.time()
+            total_time += end_time - start_time
+        return total_time/10
+    
+    def getAverageTimeDynamicProgrammingAlgorithm(self):
+        total_time = 0
+        for i in range(10):
+            start_time = time.time()
+            grid = Grid(10, 50)
+            nodes = grid.SetCover()
             end_time = time.time()
             total_time += end_time - start_time
         return total_time/10
@@ -40,3 +50,4 @@ if __name__ == "__main__":
     print("Random Algorithm:", timeComplexity.getAverageTimeRandomAlgorithm())
     print("Pure Greedy Algorithm:", timeComplexity.getAverageTimePureGreedyAlgorithm())
     print("Greedy Set Cover Algorithm:",timeComplexity.getAverageTimeGreedySetCoverAlgorithm())
+    print("Dynamic Programming Algorithm:",timeComplexity.getAverageTimeDynamicProgrammingAlgorithm())
