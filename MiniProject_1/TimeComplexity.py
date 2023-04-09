c2851ffb42db14ef8a2c7a049317d888d3595183
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-04-09 16:48:06 -0400

    Update TimeComplexity.py
    Wrong Project

--- MiniProject_1/TimeComplexity.py
+++ MiniProject_1/TimeComplexity.py
@@ -34,20 +34,9 @@ class TimeComplexity:
             end_time = time.time()
             total_time += end_time - start_time
         return total_time/10
-    
-    def getAverageTimeDynamicProgrammingAlgorithm(self):
-        total_time = 0
-        for i in range(10):
-            start_time = time.time()
-            grid = Grid(10, 50)
-            nodes = grid.SetCover()
-            end_time = time.time()
-            total_time += end_time - start_time
-        return total_time/10
-    
+     
 if __name__ == "__main__":
     timeComplexity = TimeComplexity()
     print("Random Algorithm:", timeComplexity.getAverageTimeRandomAlgorithm())
     print("Pure Greedy Algorithm:", timeComplexity.getAverageTimePureGreedyAlgorithm())
     print("Greedy Set Cover Algorithm:",timeComplexity.getAverageTimeGreedySetCoverAlgorithm())
-    print("Dynamic Programming Algorithm:",timeComplexity.getAverageTimeDynamicProgrammingAlgorithm())
